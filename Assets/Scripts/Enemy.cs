d3596dc9138ed236310333a49e47d19ee8fe677f
Kalad132 <67950598+Kalad132@users.noreply.github.com>
2020-11-30 18:48:40 +0300

    init

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Enemy : MonoBehaviour
+{
+    private Vector3[] _wayPoints;
+
+    private void Awake()
+    {
+        Transform[] pathPoints = gameObject.GetComponentsInChildren<Transform>();
+        _wayPoints = new Vector3[pathPoints.Length];
+        for (int i = 0; i<_wayPoints.Length; i++)
+            _wayPoints[i] = pathPoints[i].position;
+    }
+
+    private void Start()
+    {
+        Tween path = transform.DOPath(_wayPoints, 15, PathType.CatmullRom).SetOptions(true).SetLoops(-1);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jump;
+
+    private float _groundCheckDistance = 0.1f;
+    private int _direction;
+    private Rigidbody2D _rigidbody;
+    private SpriteRenderer _sprite;
+    private Animator _animator;
+    private BoxCollider2D _collider;
+
+    private void Awake()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+        _sprite = gameObject.GetComponent<SpriteRenderer>();
+        _rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        _collider = gameObject.GetComponent<BoxCollider2D>();
+    }
+    private void Update()
+    {
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+            _rigidbody.AddForce(new Vector2(0, _jump));
+        if (Input.GetKey(KeyCode.A))
+            SetDirection(-1);
+        else if (Input.GetKey(KeyCode.D))
+            SetDirection(1);
+        else
+            SetDirection(0);
+        Move();
+    }
+
+    private void Move()
+    {
+        gameObject.transform.Translate(new Vector2(_direction * _speed * Time.deltaTime, 0));
+    }
+
+    private void SetDirection(int direction)
+    {
+        _direction = direction;
+        if (direction == -1)
+            _sprite.flipX = true;
+        else if (direction == 1)
+            _sprite.flipX = false;
+        _animator.SetFloat("RunSpeed", Mathf.Abs(_direction));
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D[] _hit = Physics2D.BoxCastAll(_collider.bounds.min, new Vector2(0.1f, 0.1f), 0f, Vector2.down, _groundCheckDistance);
+        _animator.SetBool("Grounded", _hit.Length > 1);
+        return _hit.Length>1;
+    }
+
+
+}
+
+
+
